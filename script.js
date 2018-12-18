7de7e296917f6f41b414094bf16c0dcd0b827f1f
YonglinMai <43829014+YonglinMai@users.noreply.github.com>
2018-12-18 17:04:24 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".hi").click(function(){
+    $(".hi").text("Now Double Click The Next Picture");
+    $(".steptwo").slideDown();
+});
+$(".steptwo").dblclick(function(){
+    $(".steptwo").hide();
+    $("button").show();
+});
+$(".stepthree").mouseleave(function(){
+    $(".stepthree").text("Click");
+});
+$(".stepthree").click(function(){
+    $("p").show();
+    $("button").hide();
+    $(".final").css("color","red");
+});
+$(".final").dblclick(function(){
+    $(".whole").fadeOut();
+    $("body").css("background-color","gold");
+    $(".treat").fadeIn();
+});
